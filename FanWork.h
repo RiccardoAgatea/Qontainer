9e59174285d57aa0776375732d18def76e58d733
Riccardo Agatea <riccardo.agatea@gmail.com>
2018-12-06 15:24:24 +0100

    First commit

---
+++ FanWork.h
@@ -0,0 +1,15 @@
+#ifndef FANWORK_H
+#define FANWORK_H
+#include<QString>
+#include<QDate>
+
+class FanWork
+{
+public:
+    QString title, author;
+
+public:
+    FanWork();
+};
+
+#endif // FANWORK_H
